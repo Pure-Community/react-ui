5f23577d9f4a9af4f9da6c3689ffaf8b98c30558
BlackKingWhitePawn <riabtsev.nikita@ya.ru>
2022-10-11 15:29:37 +0500

    cleared for test package creating

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
     devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
                 exclude: /node_modules/,
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                sourceMap: true,
-                                includePaths: [path.resolve(__dirname, './src/styles/')],
-                            },
-                        },
-                    },
-                ],
-            },
+            // {
+            //     test: /\.s[ac]ss$/i,
+            //     use: [
+            //         // Creates `style` nodes from JS strings
+            //         'style-loader',
+            //         // Translates CSS into CommonJS
+            //         'css-loader',
+            //         // Compiles Sass to CSS
+            //         {
+            //             loader: 'sass-loader',
+            //             options: {
+            //                 sassOptions: {
+            //                     sourceMap: true,
+            //                     includePaths: [path.resolve(__dirname, './src/styles/')],
+            //                 },
+            //             },
+            //         },
+            //     ],
+            // },
         ],
     },
     resolve: {
@@ -37,5 +42,16 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: true,
+    },
+    optimization: {
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+    },
+    output: {
+        pathinfo: false,
     },
 }
