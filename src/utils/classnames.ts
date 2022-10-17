21a200ad1c91b95926f57cade6ce34398fa73537
BlackKingWhitePawn <riabtsev.nikita@ya.ru>
2022-10-17 18:38:08 +0500

    replace classnames utils with class object methods

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,7 +1,30 @@
-const createElementClassNameFactory = (blockClassName: string) : ((className: string) => string)[] => [
-    (className: string) => `${blockClassName}__${className}`,
-    (className: string) => `${blockClassName}_${className}`
-]
+class ClassNamesGenerator {
+    private blockCN;
+    private style: any;
 
+    constructor(blockCN: string, style: any) {
+        if (blockCN !== '') this.blockCN = blockCN
+        else throw new RangeError('Classname must be non empty string')
+        this.style = style
+    }
 
-export {createElementClassNameFactory}
+    public getBlockCN(...modificators: string[]) {
+        console.log(this.style[this.blockCN]);
+        
+        if (!modificators) return this.style[this.blockCN]
+        else return [this.style[this.blockCN]]
+        .concat(modificators
+            .map(m => this.style[`${this.blockCN}_${m}`]))
+            .join(' ')
+    }
+
+    public getElementCN(elementCN: string, ...modificators: string[]) {
+        if (!modificators) return this.style[`${this.blockCN}__${elementCN}`]
+        else return [this.style[`${this.blockCN}__${elementCN}`]]
+        .concat(modificators
+            .map(m => this.style[`${this.blockCN}__${elementCN}_${m}`]))
+            .join(' ')
+    }
+}
+
+export { ClassNamesGenerator }
